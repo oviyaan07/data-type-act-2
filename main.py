4e520fdc567560a58dd4e0b7983f009b30598a2b
oviyaan07 <oviyaansingh04@gmail.com>
2024-10-27 12:29:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+name="oviyaan"
+age=11
+weight=36.8
+isstudent=True
+print("name",name)
+print("type of name",type(name))
+print("age",age)
+print("type of age",type(age))
+print("weight",weight)
+print("type of weight",type(weight))
+print("isstudent",isstudent)
+print("type of isstudent",type(isstudent))
+age=str(age)
+print("type of age",type(age))
+weight=int(weight)
+print(weight)
